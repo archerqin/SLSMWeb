13200849f459a91b88dd39b9722e447b01aa6946
archerqin <qhx1992@gmail.com>
2021-08-23 21:30:46 +0800

    temp

--- app/base/models.py
+++ app/base/models.py
@@ -5,6 +5,28 @@ from app import db, login_manager
 
 from app.base.util import hash_pass
 
+class Permission:
+    COMMITBUG = 0x01
+    FINISHBUGS = 0x02
+    ADMINISTRATOR = 0x80
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    role_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    permission = db.Column(db.Integer)
+    user = db.relationship('User', backref='role', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User':(Permission.COMMITBUG |
+                    Permission.FINISHBUGS, True),
+            'Administrator':(0xff, False)
+        }
+        for r in roles:
+            role = Role.query
+
 class User(db.Model, UserMixin):
 
     __tablename__ = 'User'
@@ -35,3 +57,9 @@ def request_loader(request):
     username = request.form.get('username')
     user = User.query.filter_by(username=username).first()
     return user if user else None
+
+
+class Bug(db.Model):
+    __tablename__ = 'bugs'
+    bug_id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text)
