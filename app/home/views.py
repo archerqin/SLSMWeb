ee23cdf5947034c8068e26dd7d202c42dc014515
archerqin <qhx1992@gmail.com>
2021-09-07 21:15:47 +0800

    modal

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,5 @@
 from app.home import blueprint
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, make_response
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
@@ -8,4 +8,12 @@ from jinja2 import TemplateNotFound
 @login_required
 def index():
     return render_template('index.html', segment='index')
+
+
+@blueprint.route('/bug/flag=<int:flag>')
+def show_bugs(flag):
+    if flag == 1:
+        resp = make_response(redirect(url_for('.index')))
+        print("show_bugs flag1")
+        return resp
 
