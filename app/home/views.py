d11aea7a9f0e2a3759ea99f63a5860878d9a1ae4
archerqin <qhx1992@gmail.com>
2021-11-10 21:29:13 +0800

    20211110

--- app/home/views.py
+++ app/home/views.py
@@ -1,15 +1,15 @@
 from app.home import blueprint
-from flask import render_template, redirect, url_for, request, make_response
+from flask import render_template, redirect, url_for, request, make_response, jsonify, json
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
 from ..base.models import Bug
+from .. import db
 
 @blueprint.route('/index')
 @login_required
 def index():
     return render_template('index.html', segment='index')
-
 
 @blueprint.route('/bug/flag=<int:flag>')
 def show_bugs(flag):
@@ -18,7 +18,16 @@ def show_bugs(flag):
         print("show_bugs flag1")
         return resp
 
-@blueprint.route('/caseSubmit', method=['Get', 'Post'])
-def case_submit():
-    pass
-    
+@blueprint.route('/case_commit', methods=['Get', 'Post'])
+def case_commit():
+    print("case_commit")
+    # resp = make_response(redirect(url_for('.index')))
+    # return resp
+    data = json.loads(request.form.get('data'))
+    text = data['text']
+    desc = data['desc']
+    print(text, desc)
+    case = Bug(text=text, detail=desc)
+    db.session.add(case)
+
+    return jsonify(data)    
